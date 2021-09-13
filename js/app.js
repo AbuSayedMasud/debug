148e709f47691e3cc08fef6d1a9984995a7420da
AbuSayedMasud <sayedmasud1996@gmail.com>
2021-09-13 08:46:17 +0600

    last commit

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 const loadProducts = () => {
   const url = `https://fakestoreapi.com/products`;
   fetch(url)
-    .then((response) => response.json())
-    .then((data) => showProducts(data));
+    .then(response => response.json())
+    .then(data => showProducts(data));
 };
 loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
+   
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
@@ -20,8 +21,12 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <div class="span-area">
+      <span class="span">Number of peoples: ${product.rating.count}</span>
+      <span class="span"> rate: ${product.rating.rate}</span>
+      </div>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button id="details-btn" onclick="deatils(${product.id})"class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
@@ -37,7 +42,7 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -46,28 +51,32 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value.toFixed(2);
 };
 
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
   const priceConverted = getInputValue("price");
+  updateTotal();
   if (priceConverted > 200) {
     setInnerText("delivery-charge", 30);
     setInnerText("total-tax", priceConverted * 0.2);
+    updateTotal();
   }
   if (priceConverted > 400) {
     setInnerText("delivery-charge", 50);
     setInnerText("total-tax", priceConverted * 0.3);
+    updateTotal()
   }
   if (priceConverted > 500) {
     setInnerText("delivery-charge", 60);
     setInnerText("total-tax", priceConverted * 0.4);
+    updateTotal();
   }
 };
 
@@ -76,5 +85,35 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+//Details load
+const deatils = (id) => {
+  console.log(id);
+  const url = `https://fakestoreapi.com/products/${id}`;
+  console.log(url)
+  fetch(url)
+    .then(res => res.json())
+    .then(json => showDetails(json))
+}
+const showDetails = data => {
+ 
+  const div = document.createElement("div");
+  div.classList.add("details");
+  div.innerHTML = `<div class="single-product">
+      <div>
+    <img class="product-image" src=${data.image}></img>
+      </div>
+      <h3>${data.title}</h3>
+      <p>Category: ${data.category}</p>
+      <h2>Price: $ ${data.price}</h2>
+      <div class="span-area">
+      <span class="span">Number of peoples: ${data.rating.count}</span>
+      <span class="span"> rate: ${data.rating.rate}</span>
+      <p class="text">${data.description}</p>
+      </div>
+     </div>
+      `;
+  document.getElementById("details-area").appendChild(div);
+
+}
